a844aa43b430008f86fad78956a8c43e16ca8d75
RasmusJin <s201410@dtu.dk>
2022-02-15 15:24:15 +0100

    sd

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner; 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("tjek dit boomer ego: indtast alder: ");
+        Scanner input=new Scanner(System.in);
+        int alder=input.nextInt();  
+        if(alder<30){
+            System.out.println("du har desværre et boomer ego, der skal strokes.");
+
+        }
+        else{
+            System.out.println("du har ikke et boomer ego, tillykke!");
+        }
+        input.close(); 
     }
 }
--- src/test.java
+++ src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args){
         System.out.println("Test om mit commit virker");
+        System.out.println("ja lad os se om det gør");
     }
 }
